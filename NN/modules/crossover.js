134d9861a25f7846ef3a097c6a4d8fc0adaa1aa8
Suissa <jnascimento@gmail.com>
2016-11-20 17:35:00 -0200

    iniciando crossover

--- NN/modules/crossover.js
+++ NN/modules/crossover.js
@@ -5,33 +5,33 @@ const RandInt = (x, y) => Math.floor(Math.random() *(y-x+1)) + x;
 //returns a random float between zero and 1
 const RandFloat = () => Math.rand() 
 
-const CrossoverRate = 0.1
+const CROSSOVER_RATE = 0.1
+const WEIGHT_TOTAL = 10
 
-const Crossover = function(mum,dad) {
+const Crossover = (mum, dad) => {
   //just return parents as offspring dependent on the rate
   //or if parents are the same
-  if ((RandFloat() > this.m_dCrossoverRate) || (mum == dad)) {
-    baby1 = mum;
-    baby2 = dad;
+  let baby1 = []
+  let baby2 = []
 
-    return [baby1,baby2];
+  if ((RandFloat() > CROSSOVER_RATE) || (mum == dad)) {
+    return [mum, dad]
   }
-
   //determine a crossover point
-  cp = RandInt(0, this.m_iChromoLength - 1);
+  cp = RandInt(0, WEIGHT_TOTAL - 1);
 
   //create the offspring
-  for (i=0; i<cp; ++i)
-  {
-    baby1.push(mum[i]);
-    baby2.push(dad[i]);
+  for (let i=0; i < cp; ++i) {
+    baby1.push(mum[i])
+    baby2.push(dad[i])
   }
 
-  for (i=cp; i<mum.size(); ++i)
-  {
-    baby1.push(dad[i]);
-    baby2.push(mum[i]);
+  for (i=cp; i < mum.size(); ++i) {
+    baby1.push(dad[i])
+    baby2.push(mum[i])
   }
   
-  return[baby1,baby2];
-};
+  return[baby1,baby2]
+}
+
+module.exports = Crossover
